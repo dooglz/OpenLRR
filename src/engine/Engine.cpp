336a5c1878f94ea825ded0775abf7c8a73ba8298
Sam Serrels <doogly1@gmail.com>
2020-01-06 13:40:10 +0000

    avg to utils

--- src/engine/Engine.cpp
+++ src/engine/Engine.cpp
@@ -7,10 +7,8 @@
 #include "../utils.h"
 #include "graphics/vk/vulkan.h"
 #include "platform/platform_glfw.h"
-#include <algorithm>
 #include <chrono>
 #include <exception>
-#include <functional>
 #include <glm/gtc/quaternion.hpp>
 #include <iostream>
 #include <thread>
@@ -20,36 +18,7 @@ const double TARGET_DT = 1.0 / 30.0;
 size_t delay = 0;
 void baa(double a) {}
 
-struct movingAverage {
-  double val;
-  movingAverage(size_t r, std::function<void(double, double, double)> cb = nullptr) : reset{r}, cb{cb} { _reset(); }
-  double add(double in) {
-    if (count >= reset) {
-      if (cb != nullptr) {
-        cb(val, min, max);
-      }
-      _reset();
-    }
-    min = std::min(min, in);
-    max = std::max(max, in);
-    double scaling = 1. / (double)(count + 1);
-    val = in * scaling + val * (1. - scaling);
-    count++;
-    return val;
-  };
 
-private:
-  void _reset() {
-    val = 0;
-    count = 0;
-    min = std::numeric_limits<double>::max();
-    max = 0;
-  }
-  size_t count;
-  size_t reset;
-  double min, max;
-  std::function<void(double, double, double)> cb;
-};
 
 void Engine::Startup() {}
 
--- src/utils.h
+++ src/utils.h
@@ -1,9 +1,12 @@
 #ifndef OPENLRR_UTILS_H
 #define OPENLRR_UTILS_H
+#define NOMINMAX
 #include <glm/ext/quaternion_common.hpp>
 #include <glm/ext/quaternion_double.hpp>
 #include <glm/ext/vector_double3.hpp>
 #include <glm/gtc/quaternion.hpp>
+#include <functional>
+#include <algorithm>
 // Z = UP
 // X = L->R
 // Y = 0->intoScreen
@@ -25,5 +28,38 @@ static glm::dvec3 GetForwardVector(const glm::dquat &q) {
 static glm::dvec3 GetRightVector(const glm::dquat &q) {
   return glm::dvec3(1.0f - 2.0f * (q.y * q.y + q.z * q.z), 2.0f * (q.x * q.y + q.w * q.z), 2.0f * (q.x * q.z - q.w * q.y));
 }
+
+
+struct movingAverage {
+  double val;
+  movingAverage(size_t r, std::function<void(double, double, double)> cb = nullptr) : reset{r}, cb{cb} { _reset(); }
+  double add(double in) {
+    if (count >= reset) {
+      if (cb != nullptr) {
+        cb(val, min, max);
+      }
+      _reset();
+    }
+    min = std::min(min, in);
+    max = std::max(max, in);
+    double scaling = 1. / (double)(count + 1);
+    val = in * scaling + val * (1. - scaling);
+    count++;
+    return val;
+  };
+
+private:
+  void _reset() {
+    val = 0;
+    count = 0;
+    min = std::numeric_limits<double>::max();
+    max = 0;
+  }
+  size_t count;
+  size_t reset;
+  double min, max;
+  std::function<void(double, double, double)> cb;
+};
+
 
 #endif //OPENLRR_UTILS_H
